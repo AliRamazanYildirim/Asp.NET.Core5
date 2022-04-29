ef5b4fb7f01cc3bf2765db6cea0a8430a8052656
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-04-29 17:05:05 +0300

    Generische Quelle definieren

---
+++ NSchicht.Quelle/Quelle/GenerischeQuelle.cs
@@ -0,0 +1,47 @@
+﻿using NSchicht.Quelle.Abstrakt;
+using NSchicht.Quelle.Konkret;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSchicht.Quelle.Quelle
+{
+    public class GenerischeQuelle<T> : IGenerischeDüo<T> where T : class
+    {
+        public void Aktualisieren(T einheit)
+        {
+            using var w = new Kontext();
+            w.Update(einheit);
+            w.SaveChanges();
+        }
+
+        public void Insert(T einheit)
+        {
+            using var w = new Kontext();
+            w.Add(einheit);
+            w.SaveChanges();
+        }
+
+        public void Löschen(T einheit)
+        {
+            using var w = new Kontext();
+            w.Remove(einheit);
+            w.SaveChanges();
+        }
+
+        public List<T> RufAlleDaten()
+        {
+            using var w = new Kontext();
+            return w.Set<T>().ToList();
+            
+        }
+
+        public T RufZurID(int ID)
+        {
+            using var w = new Kontext();
+            return w.Set<T>().Find(ID);
+        }
+    }
+}
