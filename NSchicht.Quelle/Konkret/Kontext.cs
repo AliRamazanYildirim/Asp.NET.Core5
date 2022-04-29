c62a4b41a5dcb92ddcc89267286389ffc87556cf
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-04-29 15:44:26 +0300

    Verbindung mit DbContext konfigurieren.

---
+++ NSchicht.Quelle/Konkret/Kontext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSchicht.Quelle.Konkret
+{
+    public class Kontext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=THINKPAD;Initial Catalog=AspNetCoreProjektDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
