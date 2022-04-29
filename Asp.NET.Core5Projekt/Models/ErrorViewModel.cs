00171fe57a784a0f6b1219c16c8ac3cd3ceda3a2
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-04-29 14:30:24 +0300

    Add project files.

---
+++ Asp.NET.Core5Projekt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.NET.Core5Projekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
