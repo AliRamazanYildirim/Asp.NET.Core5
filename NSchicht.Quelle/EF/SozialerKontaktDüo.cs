f3ab5216cc1f46e8f08be43b171e9a7a80648ecc
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-04-29 17:21:54 +0300

    Entity Framework Düo definieren

---
+++ NSchicht.Quelle/EF/SozialerKontaktDüo.cs
@@ -0,0 +1,15 @@
+﻿using NSchicht.Core.Konkret;
+using NSchicht.Quelle.Abstrakt;
+using NSchicht.Quelle.Quelle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSchicht.Quelle.EF
+{
+    public class SozialerKontaktDüo:GenerischeQuelle<SozialerKontakt>,ISozialerKontaktDüo
+    {
+    }
+}
