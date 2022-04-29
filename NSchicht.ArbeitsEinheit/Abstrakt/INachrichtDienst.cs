81d11bcdd4e1dc0f07a483b3f1edce22b6362ed5
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-04-29 17:44:15 +0300

    Dienste für andere Klassen definieren

---
+++ NSchicht.ArbeitsEinheit/Abstrakt/INachrichtDienst.cs
@@ -0,0 +1,13 @@
+﻿using NSchicht.Core.Konkret;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSchicht.ArbeitsEinheit.Abstrakt
+{
+    public interface INachrichtDienst:IGenerischesDienst<Nachricht>
+    {
+    }
+}
