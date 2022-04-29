68bbc8125ccb1e5220c33cea5c5da86291251d80
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-04-29 17:30:21 +0300

    Erstellen einer Serviceschnittstelle.

---
+++ NSchicht.ArbeitsEinheit/Abstrakt/IGenerischesDienst.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSchicht.ArbeitsEinheit.Abstrakt
+{
+    public interface IGenerischesDienst<T>
+    {
+        void TInsert(T einheit);
+        void TAktualisieren(T einheit);
+        void TLöschen(T einheit);
+        List<T> TRufAlleDaten();
+        T TRufZurID(int ID);
+    }
+}
