22e728534dc6f2838e6df3f4ea22f6af7746c5bd
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-05-13 15:56:48 +0300

    Eigenschaft-Änderungen

--- Asp.NET.Core5Projekt/Controllers/EigenschaftController.cs
+++ Asp.NET.Core5Projekt/Controllers/EigenschaftController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSchicht.ArbeitsEinheit.Konkret;
+using NSchicht.Core.Konkret;
+using NSchicht.Quelle.EF;
 
 namespace Asp.NET.Core5Projekt.Controllers
 {
     public class EigenschaftController : Controller
     {
+        EigenschaftManager eigenschaftManager = new EigenschaftManager(new EfEigenschaftDüo());
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            ViewBag.w1 = "Eigenschaft Bearbeiten";
+            ViewBag.w2 = "Eigenschaft";
+            ViewBag.w3 = "Eigenschaft Bearbeiten";
+            var werte = eigenschaftManager.TRufZurID(1);
+            return View(werte);
+        }
+        
+        [HttpPost]
+        public IActionResult Index(Eigenschaft e)
+        {
+            eigenschaftManager.TAktualisieren(e);
+            return RedirectToAction("Index", "Ursprünglich");
         }
     }
 }
