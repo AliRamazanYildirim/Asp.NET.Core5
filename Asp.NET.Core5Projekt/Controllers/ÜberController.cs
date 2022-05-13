9a6550683b0cd2c97a73d99d6d9ee971723c039b
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-05-13 16:24:33 +0300

    Über-Änderung

---
+++ Asp.NET.Core5Projekt/Controllers/ÜberController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSchicht.ArbeitsEinheit.Konkret;
+using NSchicht.Core.Konkret;
+using NSchicht.Quelle.EF;
+
+namespace Asp.NET.Core5Projekt.Controllers
+{
+    public class ÜberController : Controller
+    {
+        ÜberManager überManager = new ÜberManager(new EfÜberDüo());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.w1 = "Über Bearbeiten";
+            ViewBag.w2 = "Über";
+            ViewBag.w3 = "Über Bearbeiten";
+            var werte = überManager.TRufZurID(1);
+            return View(werte);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Über ü)
+        {
+            überManager.TAktualisieren(ü);
+            return RedirectToAction("Index", "Ursprünglich");
+        }
+    }
+}
