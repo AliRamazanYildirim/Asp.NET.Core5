39dd66ca86d4ced19e97ce6e3a645e2636536e7d
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-05-14 14:38:47 +0300

    Das Thema wurde aufgrund des alten unregelmäßigen Themas geändert.

---
+++ Asp.NET.Core5Projekt/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NET.Core5Projekt.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.w1 = "Dashboard Bearbeiten";
+            ViewBag.w2 = "Dashboard";
+            ViewBag.w3 = "Dashboard Bearbeiten";
+            return View();
+        }
+    }
+}
