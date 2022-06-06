165b07e18f2b9212e736e0bd3255a8b25bd4bf7b
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-06-06 16:25:35 +0300

    Ajax Verwendung

---
+++ Asp.NET.Core5Projekt/Controllers/AjaxErfahrungController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NSchicht.ArbeitsEinheit.Konkret;
+using NSchicht.Core.Konkret;
+using NSchicht.Quelle.EF;
+
+namespace Asp.NET.Core5Projekt.Controllers
+{
+    public class AjaxErfahrungController : Controller
+    {
+        ErfahrungManager _erfahrungManager = new ErfahrungManager(new EfErfahrungDüo());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ErfahrungAuflisten()
+        {
+            var werte = JsonConvert.SerializeObject(_erfahrungManager.TRufAlleDaten());
+            return Json(werte);
+        }
+        [HttpPost]
+        public IActionResult InsertErfahrung(Erfahrung erfahrung)
+        {
+            _erfahrungManager.TInsert(erfahrung);
+            var werte = JsonConvert.SerializeObject(erfahrung);
+            return Json(werte);
+        }
+        public IActionResult RufZurID(int ErfahrungID)
+        {
+            var wert = _erfahrungManager.TRufZurID(ErfahrungID);
+            var werte = JsonConvert.SerializeObject(wert);
+            return Json(werte);
+        }
+        public IActionResult LöschenErfahrung(int ID)
+        {
+            var wert = _erfahrungManager.TRufZurID(ID);
+            _erfahrungManager.TLöschen(wert);
+            return NoContent();
+        }
+        public IActionResult AktualisierenErfahrung(Erfahrung erfahrung)
+        {
+            var wert = _erfahrungManager.TRufZurID(erfahrung.ErfahrungID);
+            _erfahrungManager.TAktualisieren(wert);
+            var werte = JsonConvert.SerializeObject(erfahrung);
+            return Json(werte);
+        }
+    }
+}
---
+++ NSchicht.ArbeitsEinheit/Konkret/AutorAnwenderManager.cs
@@ -0,0 +1,51 @@
+﻿using NSchicht.ArbeitsEinheit.Abstrakt;
+using NSchicht.Core.Konkret;
+using NSchicht.Quelle.Abstrakt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSchicht.ArbeitsEinheit.Konkret
+{
+    public class AutorAnwenderManager : IAutorAnwenderDienst
+    {
+        private readonly IAutorAnwenderDüo _autorAnwenderDüo;
+
+        public AutorAnwenderManager(IAutorAnwenderDüo autorAnwenderDüo)
+        {
+            _autorAnwenderDüo = autorAnwenderDüo;
+        }
+
+        public void TAktualisieren(IdentityBenutzer einheit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(IdentityBenutzer einheit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TLöschen(IdentityBenutzer einheit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IdentityBenutzer> TRufAlleDaten()
+        {
+            return _autorAnwenderDüo.RufAlleDaten();
+        }
+
+        public List<IdentityBenutzer> TRufListeZurFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IdentityBenutzer TRufZurID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
